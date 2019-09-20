a86525e26e45a35c05b1f12d4d5107f3a28702e8
Dmitry G <isublsv@gmail.com>
2019-09-20 19:35:55 +0300

    Changed constructor arguments

--- playroom/src/main/java/by/gartsmanovich/javawebdev/playroom/bean/PlayRoom.java
+++ playroom/src/main/java/by/gartsmanovich/javawebdev/playroom/bean/PlayRoom.java
@@ -2,7 +2,6 @@ package by.gartsmanovich.javawebdev.playroom.bean;
 
 import by.gartsmanovich.javawebdev.playroom.bean.toy.Toy;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PlayRoom {
@@ -13,11 +12,6 @@ public class PlayRoom {
     private double budget;
 
     /**
-     * The path to file storage.
-     */
-    private String path;
-
-    /**
      * The storage that contains an list of entities.
      */
     private List<Toy> toyStorage;
@@ -26,12 +20,11 @@ public class PlayRoom {
      * Constructs an instance of an entity with several parameters.
      *
      * @param budgetValue the budget of a play room.
-     * @param pathValue the path to the file storage.
+     * @param toyStorageValue the list of entities.
      */
-    public PlayRoom(final double budgetValue, final String pathValue) {
+    public PlayRoom(final double budgetValue, final List<Toy> toyStorageValue) {
         this.budget = budgetValue;
-        this.path = pathValue;
-        this.toyStorage = new ArrayList<>();
+        this.toyStorage = toyStorageValue;
     }
 
     /**
@@ -50,24 +43,6 @@ public class PlayRoom {
      */
     public void setBudget(final double budgetValue) {
         this.budget = budgetValue;
-    }
-
-    /**
-     * Gets path.
-     *
-     * @return value of path.
-     */
-    public String getPath() {
-        return path;
-    }
-
-    /**
-     * Sets path.
-     *
-     * @param pathValue value of path.
-     */
-    public void setPath(final String pathValue) {
-        path = pathValue;
     }
 
     /**
