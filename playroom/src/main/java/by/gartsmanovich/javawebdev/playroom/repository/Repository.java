9e5aedd2974158869c40c56aa28f767eb182d577
Dmitry G <isublsv@gmail.com>
2019-09-20 22:47:24 +0300

    Exception handling

--- playroom/src/main/java/by/gartsmanovich/javawebdev/playroom/repository/Repository.java
+++ playroom/src/main/java/by/gartsmanovich/javawebdev/playroom/repository/Repository.java
@@ -1,6 +1,8 @@
 package by.gartsmanovich.javawebdev.playroom.repository;
 
 import by.gartsmanovich.javawebdev.playroom.bean.Entity;
+import by.gartsmanovich.javawebdev.playroom.repository.exception
+        .RepositoryException;
 import by.gartsmanovich.javawebdev.playroom.repository.specification
         .Specification;
 
@@ -12,36 +14,52 @@ public interface Repository<T extends Entity> {
      * Creates the play room storage.
      *
      * @param budget the budget of play room.
+     * @param path the path to storage file.
+     * @param delimiter the delimiter to parse the data from file.
+     * @throws RepositoryException if error happens during execution.
      */
-    void createStorage(double budget);
+    void createStorage(double budget, String path, String delimiter)
+            throws RepositoryException;
 
     /**
-     * Adds an entity to the storage.
+     * Adds an entity to the end of the storage.
      *
      * @param entity the entity to add.
+     * @return true if operation was completed successful, false - otherwise.
      */
-    void add(T entity);
+    boolean add(T entity);
 
     /**
      * Updates an entity by ID from the storage.
      *
      * @param entity the entity to update.
+     * @return true if operation was completed successful, false - otherwise.
      */
-    void update(T entity);
+    boolean update(T entity);
 
     /**
-     * Removes an entity by ID of provided object from the storage.
+     * Removes the first occurrence of the specified element from the storage,
+     * if it is present.
      *
      * @param entity the entity to remove.
+     * @return true if operation was completed successful, false - otherwise.
      */
-    void remove(T entity);
+    boolean remove(T entity);
+
+    /**
+     * Saves repository storage to file.
+     *
+     * @param path the path to file to save.
+     * @throws RepositoryException if error happens during execution.
+     */
+    void saveStorage(String path) throws RepositoryException;
 
     /**
      * Determines the business rules that applies to the storage data.
      *
      * @param specification the concrete specification that query different
      *                      types of actions.
-     * @return the specified list of entities.
+     * @return the specified list of entities or empty list.
      */
     List<T> query(Specification<T> specification);
 }
