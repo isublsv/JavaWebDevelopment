6ca422cf76dc298a0c527210186a84e6206c5ed0
Dmitry G <isublsv@gmail.com>
2019-09-26 00:09:25 +0300

    Added examples

---
+++ demothreads/src/main/java/by/epam/thread/helloworld/ex08/BufferThread.java
@@ -0,0 +1,47 @@
+package by.epam.thread.helloworld.ex08;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+public class BufferThread {
+
+    private static final Logger LOGGER = LogManager.getLogger(BufferThread.class);
+
+    private static int counter = 0;
+    private static final StringBuilder s = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            synchronized (s) {
+                while (BufferThread.counter++ < 3) {
+                    s.append("A");
+                    String message = "> " + counter + " ";
+                    LOGGER.debug(message);
+                    LOGGER.debug(BufferThread.s);
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(500);
+                    } catch (InterruptedException eValue) {
+                        LOGGER.error(eValue);
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        }).start();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException eValue) {
+            LOGGER.error(eValue);
+            Thread.currentThread().interrupt();
+        }
+        while (BufferThread.counter++ < 6) {
+            String message = "< " + counter + " ";
+            LOGGER.debug(message);
+            BufferThread.s.append("B");
+            LOGGER.debug(BufferThread.s);
+        }
+    }
+}
