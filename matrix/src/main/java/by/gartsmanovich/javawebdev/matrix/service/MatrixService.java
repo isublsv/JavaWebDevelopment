117d8ca40a11b65bb2b1282b6ba7012b19bb79d8
Dmitry G <isublsv@gmail.com>
2019-10-01 17:51:49 +0300

    changed the method return types. fixed javadocs

--- matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/service/MatrixService.java
+++ matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/service/MatrixService.java
@@ -1,13 +1,13 @@
 package by.gartsmanovich.javawebdev.matrix.service;
 
-import by.gartsmanovich.javawebdev.matrix.bean.Matrix;
 import by.gartsmanovich.javawebdev.matrix.service.exception.ServiceException;
 
 public interface MatrixService {
 
     /**
-     * Creates the Matrix instance. Fills it from file by integers that divided
-     * by provided delimiter.
+     * Creates the Matrix instance. Gets the number of active threads, the
+     * array of values for the main diagonal of the matrix and the values for
+     * matrix array from file. Divide data in the file by provided delimiter.
      *
      * @param path      the path to storage file.
      * @param delimiter the delimiter to parse the data from file.
@@ -16,44 +16,38 @@ public interface MatrixService {
     void createMatrix(String path, String delimiter) throws ServiceException;
 
     /**
+     * Returns the array of integers that main diagonal was filled by using
+     * synchronised construction.
+     *
+     * @return the array of integers with a filled main diagonal.
+     * @throws ServiceException if error happens during execution.
+     */
+    int[][] doOption1() throws ServiceException;
+
+    /**
+     * Returns the array of integers that main diagonal was filled by using
+     * Lock classes.
+     *
+     * @return the array of integers with a filled main diagonal.
+     * @throws ServiceException if error happens during execution.
+     */
+    int[][] doOption2() throws ServiceException;
+
+    /**
+     * Returns the array of integers that main diagonal was filled by using
+     * Executor Service class.
+     *
+     * @return the array of integers with a filled main diagonal.
+     * @throws ServiceException if error happens during execution.
+     */
+    int[][] doOption3() throws ServiceException;
+
+    /**
      * Returns the matrix that main diagonal was filled by using synchronised
      * construction.
      *
      * @return the matrix with a filled main diagonal.
      * @throws ServiceException if error happens during execution.
      */
-    Matrix doOption1() throws ServiceException;
-
-    /**
-     * Returns the matrix that main diagonal was filled by using Lock classes.
-     *
-     * @return the matrix with a filled main diagonal.
-     * @throws ServiceException if error happens during execution.
-     */
-    Matrix doOption2() throws ServiceException;
-
-    /**
-     * Returns the matrix that main diagonal was filled by using synchronised
-     * construction.
-     *
-     * @return the matrix with a filled main diagonal.
-     * @throws ServiceException if error happens during execution.
-     */
-    Matrix doOption3() throws ServiceException;
-
-    /**
-     * Returns the matrix that main diagonal was filled by using synchronised
-     * construction.
-     *
-     * @return the matrix with a filled main diagonal.
-     * @throws ServiceException if error happens during execution.
-     */
-    Matrix doOption4() throws ServiceException;
-
-    /**
-     * Sets all elements under the main diagonal of the matrix to zero.
-     *
-     * @throws ServiceException if error happens during execution.
-     */
-    void resetMatrix() throws ServiceException;
+    int[][] doOption4() throws ServiceException;
 }
