1ab9db3794a0e0d86ce4837c0f45fc9e96f8f6dd
Dmitry G <isublsv@gmail.com>
2019-10-01 17:50:38 +0300

    changed the type of response

--- matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/controller/command/impl/Option1.java
+++ matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/controller/command/impl/Option1.java
@@ -1,6 +1,5 @@
 package by.gartsmanovich.javawebdev.matrix.controller.command.impl;
 
-import by.gartsmanovich.javawebdev.matrix.bean.Matrix;
 import by.gartsmanovich.javawebdev.matrix.controller.command.Command;
 import by.gartsmanovich.javawebdev.matrix.service.MatrixService;
 import by.gartsmanovich.javawebdev.matrix.service.exception.ServiceException;
@@ -8,12 +7,20 @@ import by.gartsmanovich.javawebdev.matrix.service.factory.ServiceFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Option1 implements Command {
 
     /**
-     * The logger for Exit class.
+     * The logger for Option1 class.
      */
     private static final Logger LOGGER = LogManager.getLogger(Option1.class);
+
+    /**
+     * The default delimiter.
+     */
+    private static final String DEL = " ";
 
     /**
      * Handles the request parameters and passes its to the Service application
@@ -25,14 +32,21 @@ public class Option1 implements Command {
      */
     @Override
     public String execute(final String request) {
+
         String response;
 
         ServiceFactory serviceFactory = ServiceFactory.getInstance();
         MatrixService matrixService = serviceFactory.getMatrixService();
 
         try {
-            Matrix matrix = matrixService.doOption1();
-            response = matrix.toString();
+            int[][] array = matrixService.doOption1();
+
+            response = Arrays.stream(array)
+                             .map(s -> Arrays.stream(s)
+                                             .mapToObj(String::valueOf)
+                                             .collect(Collectors.joining(DEL)))
+                             .collect(Collectors.joining(
+                                     System.lineSeparator()));
         } catch (ServiceException e) {
             response = e.getMessage();
             LOGGER.error(response);
--- matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/controller/command/impl/Option3.java
+++ matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/controller/command/impl/Option3.java
@@ -1,6 +1,5 @@
 package by.gartsmanovich.javawebdev.matrix.controller.command.impl;
 
-import by.gartsmanovich.javawebdev.matrix.bean.Matrix;
 import by.gartsmanovich.javawebdev.matrix.controller.command.Command;
 import by.gartsmanovich.javawebdev.matrix.service.MatrixService;
 import by.gartsmanovich.javawebdev.matrix.service.exception.ServiceException;
@@ -8,12 +7,20 @@ import by.gartsmanovich.javawebdev.matrix.service.factory.ServiceFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Option3 implements Command {
 
     /**
-     * The logger for Exit class.
+     * The logger for Option3 class.
      */
     private static final Logger LOGGER = LogManager.getLogger(Option3.class);
+
+    /**
+     * The default delimiter.
+     */
+    private static final String DEL = " ";
 
     /**
      * Handles the request parameters and passes its to the Service application
@@ -31,8 +38,14 @@ public class Option3 implements Command {
         MatrixService matrixService = serviceFactory.getMatrixService();
 
         try {
-            Matrix matrix = matrixService.doOption3();
-            response = matrix.toString();
+            int[][] array = matrixService.doOption3();
+
+            response = Arrays.stream(array)
+                             .map(s -> Arrays.stream(s)
+                                             .mapToObj(String::valueOf)
+                                             .collect(Collectors.joining(DEL)))
+                             .collect(Collectors.joining(
+                                     System.lineSeparator()));
         } catch (ServiceException e) {
             response = e.getMessage();
             LOGGER.error(response);
