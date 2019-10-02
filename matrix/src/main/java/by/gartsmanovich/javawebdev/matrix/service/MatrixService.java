6fdfe995e65cd6613210d0a991d54559f78e9dee
Dmitry G <isublsv@gmail.com>
2019-10-02 20:14:04 +0300

    rename methods

--- matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/service/MatrixService.java
+++ matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/service/MatrixService.java
@@ -22,7 +22,7 @@ public interface MatrixService {
      * @return the array of integers with a filled main diagonal.
      * @throws ServiceException if error happens during execution.
      */
-    int[][] doOption1() throws ServiceException;
+    int[][] fillBySeparateThreads() throws ServiceException;
 
     /**
      * Returns the array of integers that main diagonal was filled by using
@@ -40,14 +40,13 @@ public interface MatrixService {
      * @return the array of integers with a filled main diagonal.
      * @throws ServiceException if error happens during execution.
      */
-    int[][] doOption3() throws ServiceException;
+    int[][] fillByExecutorService() throws ServiceException;
 
     /**
-     * Returns the matrix that main diagonal was filled by using synchronised
-     * construction.
+     * Returns the matrix that main diagonal was filled by using semaphore.
      *
      * @return the matrix with a filled main diagonal.
      * @throws ServiceException if error happens during execution.
      */
-    int[][] doOption4() throws ServiceException;
+    int[][] fillBySemaphore() throws ServiceException;
 }
