66c5675b6cc44033abe1d1bd61d01796e44567b3
Dmitry G <isublsv@gmail.com>
2019-10-05 14:25:26 +0300

    full rework

--- matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/bean/thread/SemaphoreMatrixThread.java
+++ matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/bean/thread/SemaphoreMatrixThread.java
@@ -1,11 +1,19 @@
 package by.gartsmanovich.javawebdev.matrix.bean.thread;
 
+import by.gartsmanovich.javawebdev.matrix.bean.BasicThread;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.Semaphore;
 
-public class SemaphoreMatrixThread implements Runnable {
+/**
+ * Class used to write provided values to the 2d array using
+ * {@link java.util.concurrent.Semaphore} technology in the separate
+ * thread.
+ *
+ * @author Dmitry Gartsmanovich
+ */
+public class SemaphoreMatrixThread extends BasicThread implements Runnable {
 
     /**
      * The logger for SemaphoreMatrixThread class.
@@ -18,28 +26,21 @@ public class SemaphoreMatrixThread implements Runnable {
      * array.
      */
     private Semaphore semaphore;
-    /**
-     * Contains the array instance of integers.
-     */
-    private int[][] array;
-
-    /**
-     * Contains a new diagonal's value of the 2-d array.
-     */
-    private int value;
 
     /**
      * Constructs the new thread with specific parameters.
      *
-     * @param semaphoreValue the provided semaphore instance.
+     * @param idValue the ID of the thread.
+     * @param nameValue the name of the thread.
      * @param arrayValue the array instance of integers.
-     * @param newValue   a new diagonal's value of the 2-d array.
+     * @param diagValues the array instance with diagonal values.
+     * @param semaphoreValue the provided semaphore instance.
      */
-    public SemaphoreMatrixThread(final Semaphore semaphoreValue,
-            final int[][] arrayValue, final int newValue) {
+    public SemaphoreMatrixThread(final int idValue, final String nameValue,
+            final int[][] arrayValue, final int[] diagValues,
+            final Semaphore semaphoreValue) {
+        super(idValue, nameValue, arrayValue, diagValues);
         semaphore = semaphoreValue;
-        array = arrayValue;
-        value = newValue;
     }
 
     /**
@@ -48,24 +49,23 @@ public class SemaphoreMatrixThread implements Runnable {
      */
     @Override
     public void run() {
-        String threadName = Thread.currentThread().getName();
         try {
             semaphore.acquire();
-                for (int i = 0; i < array.length; i++) {
-                    if (array[i][i] == 0) {
-                        array[i][i] = value;
-                        String message =
-                                threadName + " has insert "
-                                + value + " at " + i + " position. "
+            for (int i = 0; i < getArray().length; i++) {
+                if (getArray()[i][i] == 0) {
+                    getArray()[i][i] = getValues()[getId()];
+                    String message =
+                            getName() + " has insert " + getValues()[getId()]
+                            + " at " + i + " position. "
                                 + " Available permits: " + semaphore
                                         .availablePermits();
                         LOGGER.debug(message);
-                        break;
+                    break;
                     }
                 }
             semaphore.release();
         } catch (InterruptedException e) {
-            String errorMessage = threadName + " was interrupted";
+            String errorMessage = getName() + " was interrupted";
             LOGGER.error(errorMessage);
             Thread.currentThread().interrupt();
         }
--- matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/repository/specification/fill/FillBySemaphoreSpecification.java
+++ matrix/src/main/java/by/gartsmanovich/javawebdev/matrix/repository/specification/fill/FillBySemaphoreSpecification.java
@@ -7,33 +7,47 @@ import by.gartsmanovich.javawebdev.matrix.repository.specification
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Specification represents the solution of the task that using
+ * {@link java.util.concurrent.Semaphore} realisation to distribute
+ * provided vales between active threads and put them into the provided 2d
+ * array.
+ *
+ * @author Dmitry Gartsmanovich
+ */
 public class FillBySemaphoreSpecification implements Specification {
 
     /**
      * The timeout for the set a new value operation.
      */
-    private static final int TIMEOUT = 100;
+    private static final int TIMEOUT = 500;
 
     /**
      * Applies specified criteria to the provided array.
      *
      * @param threadNumber the number of active threads.
      * @param values       the array of provided values for the main diagonal.
-     * @param array        with provided integers.
-     * @return the array that correspond to specified criteria,
-     * false - otherwise.
+     * @param array        the 2d array with provided integers.
+     * @return the 2d array that correspond to specified criteria.
      */
     @Override
     public int[][] specified(final int threadNumber, final int[] values,
             final int[][] array) {
 
-        Semaphore semaphore = new Semaphore(threadNumber, true);
+        int permitNumber = array.length / threadNumber + 1;
+        Semaphore semaphore = new Semaphore(permitNumber, true);
 
         int[][] copy = getCopy(array);
 
-        for (int value : values) {
-            new Thread(
-                    new SemaphoreMatrixThread(semaphore, copy, value)).start();
+        int currentID = 0;
+        for (int i = 0; i < copy.length; i++) {
+            currentID++;
+            if (currentID == threadNumber) {
+                currentID = 0;
+            }
+            new Thread(new SemaphoreMatrixThread(currentID, "SemaphoreThread",
+                                                 copy, values,
+                                                 semaphore)).start();
         }
 
         try {
