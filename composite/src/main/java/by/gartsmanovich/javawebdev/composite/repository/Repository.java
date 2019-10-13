543168868f4affe642519598846ff15ec70d9085
Dmitry G <isublsv@gmail.com>
2019-10-13 21:40:15 +0300

    full rework

--- composite/src/main/java/by/gartsmanovich/javawebdev/composite/repository/Repository.java
+++ composite/src/main/java/by/gartsmanovich/javawebdev/composite/repository/Repository.java
@@ -14,29 +14,25 @@ import by.gartsmanovich.javawebdev.composite.repository.specification
 public interface Repository {
 
     /**
-     * Creates the matrix instance.
+     * Creates the composite instance.
      *
      * @param path      the path to storage file.
-     * @param delimiter the delimiter to parse the data from file.
      * @throws RepositoryException if error happens during execution.
      */
-    void createMatrix(String path, String delimiter) throws
-            RepositoryException;
+    void createComposite(String path) throws RepositoryException;
 
     /**
      * Determines the business rules that applies to the matrix.
      *
      * @param specification the concrete specification that query different
      *                      types of actions.
-     * @return the 2-d array of integers that main diagonal was filled by
-     * provided values.
+     * @return the string that contains result of query execution.
      * @throws RepositoryException if error happens during execution.
      */
-    int[][] query(Specification specification) throws RepositoryException;
+    String query(Specification specification) throws RepositoryException;
 
     /**
-     * Saves the last result obtained after executing the any method
-     * that fill the main diagonal.
+     * Saves the last result obtained after executing the any method.
      *
      * @param path the path to storage file.
      * @throws RepositoryException if error happens during execution.
