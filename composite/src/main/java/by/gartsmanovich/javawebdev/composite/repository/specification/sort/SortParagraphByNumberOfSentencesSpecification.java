d8c85b725e5c08d62131459a86d9019875c8cdb2
Dmitry G <isublsv@gmail.com>
2019-10-15 13:40:12 +0300

    sort paragraphs realisation

--- composite/src/main/java/by/gartsmanovich/javawebdev/composite/repository/specification/sort/SortParagraphByNumberOfSentencesSpecification.java
+++ composite/src/main/java/by/gartsmanovich/javawebdev/composite/repository/specification/sort/SortParagraphByNumberOfSentencesSpecification.java
@@ -1,7 +1,6 @@
 package by.gartsmanovich.javawebdev.composite.repository.specification.sort;
 
 import by.gartsmanovich.javawebdev.composite.bean.Component;
-import by.gartsmanovich.javawebdev.composite.bean.ComponentType;
 import by.gartsmanovich.javawebdev.composite.repository.specification
         .Specification;
 
@@ -37,23 +36,18 @@ public class SortParagraphByNumberOfSentencesSpecification implements
     /**
      * Applies specified criteria to the provided element.
      *
-     * @param type        the provided type of elements.
+     * @param component        the provided type of elements.
      * @return the result string that correspond to specified criteria
      */
     @Override
-    public String specified(final Component type) {
-        List<Component> paragraphs = new ArrayList<>();
+    public String specified(final Component component) {
 
-        for (Component component : type.getComponents()) {
-            if (component.getType() == ComponentType.PARAGRAPH) {
-                paragraphs.add(component);
-            }
-        }
+        List<Component> paragraphs = new ArrayList<>(
+                component.getComponents());
 
-        List<Component> collect = paragraphs.stream()
-                                            .sorted(comparator)
-                                            .collect(Collectors.toList());
-
-        return collect.toString();
+        return paragraphs.stream()
+                         .sorted(comparator)
+                         .map(Component::collect)
+                         .collect(Collectors.joining(System.lineSeparator()));
     }
 }
