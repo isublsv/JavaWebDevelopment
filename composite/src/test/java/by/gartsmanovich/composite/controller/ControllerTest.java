bb283addf6e07ef67622bb701c40e0291a281074
Dmitry G <isublsv@gmail.com>
2019-10-16 17:02:10 +0300

    added tests description

--- composite/src/test/java/by/gartsmanovich/composite/controller/ControllerTest.java
+++ composite/src/test/java/by/gartsmanovich/composite/controller/ControllerTest.java
@@ -6,32 +6,40 @@ import org.testng.annotations.Test;
 public class ControllerTest {
 
     private Controller controller;
-    private String pathToRead = "data/input.txt";
-    private String pathToWrite = "data/output.txt";
 
     @BeforeMethod
     public void setUp() {
         controller = new Controller();
+
+        String pathToRead = "data/input.txt";
+        System.out.println("COMPOSITE CREATION FROM FILE: " + pathToRead);
         controller.executeTask("1 " + pathToRead);
     }
 
     @Test
     public void testSortParagraphsByNumberOfSentences() {
+        System.out.println("SORT PARAGRAPHS BY NUMBER OF SENTENCES:");
         System.out.println(controller.executeTask("2"));
     }
 
     @Test
     public void testSortByWordLength() {
+        System.out.println("SORT WORDS BY LENGTH:");
         System.out.println(controller.executeTask("3"));
     }
 
     @Test
     public void testSortLexemesByCharNumber() {
-        System.out.println(controller.executeTask("4 a"));
+        char letter = 'a';
+        System.out.println("SORT LEXEMES BY NUMBER OF PROVIDED LETTER " + letter +
+                           " THAN BY ALPHABET:");
+        System.out.println(controller.executeTask("4 " + letter));
     }
 
     @Test
     public void testSaveComposite() {
+        String pathToWrite = "data/output.txt";
+        System.out.println("SAVE COMPOSITE INTO THE FILE: " + pathToWrite);
         System.out.println(controller.executeTask("5 " + pathToWrite));
     }
 }
