ab92b4b917ebde209aca2a566b02e9e87d6b30aa
Dmitry G <isublsv@gmail.com>
2019-10-17 12:16:49 +0300

    added new method isNumber

--- composite/src/main/java/by/gartsmanovich/composite/service/validator/Validator.java
+++ composite/src/main/java/by/gartsmanovich/composite/service/validator/Validator.java
@@ -16,4 +16,23 @@ public class Validator {
     public boolean isValidValue(final String value) {
         return value == null || value.isEmpty();
     }
+
+    /**
+     * Checks if the value is a number.
+     *
+     * @param value the value to valid.
+     * @return true if value is valid, false - otherwise.
+     */
+    public boolean isNumber(final String value) {
+        if (isValidValue(value)) {
+            try {
+                Integer.parseInt(value);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
