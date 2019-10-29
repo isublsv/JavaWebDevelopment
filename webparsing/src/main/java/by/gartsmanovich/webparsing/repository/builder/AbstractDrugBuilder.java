a403d5ab13bc9eb5949a5433ab430eb4e12800d3
Dmitry G <isublsv@gmail.com>
2019-10-29 19:09:26 +0300

    changed to list

--- webparsing/src/main/java/by/gartsmanovich/webparsing/repository/builder/AbstractDrugBuilder.java
+++ webparsing/src/main/java/by/gartsmanovich/webparsing/repository/builder/AbstractDrugBuilder.java
@@ -3,20 +3,52 @@ package by.gartsmanovich.webparsing.repository.builder;
 import by.gartsmanovich.webparsing.bean.Drug;
 import by.gartsmanovich.webparsing.repository.exception.RepositoryException;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * The base class defines common interface for all builders in the application.
+ *
+ * @author Dmitry Gartsmanovich
+ */
 public abstract class AbstractDrugBuilder {
-    private Set<Drug> drugs;
 
-    public AbstractDrugBuilder() {
-        this.drugs = new HashSet<>();
+    /**
+     * List of drug entities.
+     */
+    private List<Drug> drugs;
+
+    /**
+     * The default constructor.
+     */
+    protected AbstractDrugBuilder() {
+        this.drugs = new ArrayList<>();
     }
 
-    public Set<Drug> getDrugs() {
+    /**
+     * Sets drugs.
+     *
+     * @param drugsValue value of drugs.
+     */
+    public void setDrugs(final List<Drug> drugsValue) {
+        drugs = drugsValue;
+    }
+
+    /**
+     * Gets drugs.
+     *
+     * @return value of drugs.
+     */
+    public List<Drug> getDrugs() {
         return drugs;
     }
 
+    /**
+     * Builds the set of drug entities using DOM, SAX or StAX parsers.
+     *
+     * @param filename the provided xml-document.
+     * @throws RepositoryException if error happens during execution.
+     */
     public abstract void buildSetDrugs(String filename) throws
             RepositoryException;
 }
