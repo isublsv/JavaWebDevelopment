534622e75aa0759887df91fc24a9b29aecf0779d
Dmitry G <isublsv@gmail.com>
2019-11-02 07:57:41 +0300

    added filename checking

--- webparsing/src/main/java/by/gartsmanovich/webparsing/controller/command/impl/BuilderCommand.java
+++ webparsing/src/main/java/by/gartsmanovich/webparsing/controller/command/impl/BuilderCommand.java
@@ -10,7 +10,14 @@ import by.gartsmanovich.webparsing.service.factory.ServiceFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 /**
@@ -29,31 +36,76 @@ public class BuilderCommand implements Command {
             BuilderCommand.class);
 
     /**
+     * Sets the error message attribute.
+     */
+    private static final String ERROR_MESSAGE = "errorMessage";
+
+    /**
      * Handles the request parameters and passes its to the Service application
      * layer.
      *
      * @param request the provide request information for HTTP servlets.
      * @return the result string of correct or incorrect execution of the
      * command.
+     * @throws IOException      if an input or output error is
+     *                          detected when the servlet handles
+     *                          the request
+     * @throws ServletException if the request for the GET or POST
+     *                          could not be handled
      */
     @Override
-    public String execute(final HttpServletRequest request) {
+    public String execute(final HttpServletRequest request) throws IOException,
+            ServletException {
 
         String page;
+
+        String uploadPath = request.getServletContext()
+                                   .getResource("")
+                                   .getPath() + request.getServletContext()
+                                                       .getInitParameter(
+                                                               "upload");
+
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdir();
+        }
+
+        Part filePart = request.getPart("file");
+        String fileName = filePart.getSubmittedFileName();
+
+        if (fileName.isEmpty()) {
+            request.setAttribute(ERROR_MESSAGE, MessageManager.getProperty(
+                    "message.empty.filename"));
+            page = ConfigurationManager.getProperty("path.page.index");
+            return page;
+        } else if (!fileName.endsWith("xml")) {
+            request.setAttribute(ERROR_MESSAGE, MessageManager.getProperty(
+                    "message.not.valid.file"));
+            page = ConfigurationManager.getProperty("path.page.index");
+            return page;
+        }
+
+        File file = new File(uploadDir, fileName);
+
+        try (InputStream input = filePart.getInputStream()) {
+            Files.copy(input, file.toPath(),
+                       StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        String key = request.getParameter("key");
+        String path = file.getAbsolutePath();
+        String xsd = request.getServletContext().getResource("").getPath();
 
         ServiceFactory serviceFactory = ServiceFactory.getInstance();
         DrugService drugService = serviceFactory.getDrugService();
 
-        String key = request.getParameter("key");
-        String path = request.getParameter("path");
-
         try {
-            List<Drug> drugs = drugService.executeBuilder(key, path);
+            List<Drug> drugs = drugService.executeBuilder(key, path, xsd);
             request.setAttribute("drugs", drugs);
             page = ConfigurationManager.getProperty("path.page.result");
         } catch (ServiceException e) {
             LOGGER.error(e.getMessage());
-            request.setAttribute("errorMessage", MessageManager.getProperty(
+            request.setAttribute(ERROR_MESSAGE, MessageManager.getProperty(
                     "message.null.page"));
             page = ConfigurationManager.getProperty("path.page.error");
         }
