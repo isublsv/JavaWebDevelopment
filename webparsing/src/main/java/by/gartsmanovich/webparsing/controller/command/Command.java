95133a31625784bc89283bbca9b3bdd4ea9cfe89
Dmitry G <isublsv@gmail.com>
2019-11-02 07:57:01 +0300

    added javadoc for exceptions

--- webparsing/src/main/java/by/gartsmanovich/webparsing/controller/command/Command.java
+++ webparsing/src/main/java/by/gartsmanovich/webparsing/controller/command/Command.java
@@ -1,6 +1,8 @@
 package by.gartsmanovich.webparsing.controller.command;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * Interface for Commands.
@@ -16,6 +18,12 @@ public interface Command {
      * @param request the provide request information for HTTP servlets.
      * @return the result string of correct or incorrect execution of the
      * command.
+     * @throws IOException      if an input or output error is
+     *                          detected when the servlet handles
+     *                          the request
+     * @throws ServletException if the request for the GET or POST
+     *                          could not be handled
      */
-    String execute(HttpServletRequest request);
+    String execute(HttpServletRequest request) throws IOException,
+            ServletException;
 }
