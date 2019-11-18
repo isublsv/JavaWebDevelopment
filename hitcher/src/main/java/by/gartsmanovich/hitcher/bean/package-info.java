2456f1e341ab7b2bda1fa3bcc5e53c7b11273227
Dmitry G <isublsv@gmail.com>
2019-11-18 15:31:31 +0300

    added javadocs

--- hitcher/src/main/java/by/gartsmanovich/hitcher/bean/package-info.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/bean/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Package contains bean classes used to store data from database.
+ * Package contains bean classes is used to store data from database.
  */
 package by.gartsmanovich.hitcher.bean;
