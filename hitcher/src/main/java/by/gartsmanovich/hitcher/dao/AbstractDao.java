547e38e44f20f7608bde004afb1250e5831259c2
Dmitry G <isublsv@gmail.com>
2019-11-18 15:25:56 +0300

    added Optional support

--- hitcher/src/main/java/by/gartsmanovich/hitcher/dao/AbstractDao.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/dao/AbstractDao.java
@@ -3,15 +3,13 @@ package by.gartsmanovich.hitcher.dao;
 import by.gartsmanovich.hitcher.bean.Entity;
 import by.gartsmanovich.hitcher.dao.exception.DaoException;
 
-import java.sql.SQLException;
-import java.util.Collection;
 import java.util.Optional;
 
 /**
  * DAO interface describes the common structure for interfaces for different
  * types of entities.
  *
- * @param <T> the type of entities the DAO interface used for.
+ * @param <T> the type of entities the DAO interface is used for.
  * @author Dmitry Gartsmanovich
  */
 public interface AbstractDao<T extends Entity> {
@@ -20,35 +18,32 @@ public interface AbstractDao<T extends Entity> {
      * Saves the provided entity in the data source.
      *
      * @param entity the provided entity.
+     * @throws DaoException if failed to create entity in the data source.
      */
-    void create(T entity) throws DaoException, SQLException;
+    void create(T entity) throws DaoException;
 
     /**
      * Finds entity in the data source by provided ID if present.
      *
      * @param id the provided ID.
      * @return the entity value if present.
+     * @throws DaoException if failed to find entity by ID.
      */
-    Optional<T> findById(long id) throws SQLException, DaoException;
+    Optional<T> findById(long id) throws DaoException;
 
     /**
      * Updates the provided entity in the data source.
      *
      * @param entity the provided entity.
+     * @throws DaoException if failed to update entity in the data source.
      */
-    void update(T entity) throws DaoException, SQLException;
+    void update(T entity) throws DaoException;
 
     /**
-     * Deletes the provided entity from the data source.
+     * Deletes the provided entity by ID from the data source.
      *
-     * @param entity the provided entity.
+     * @param id the provided entity ID.
+     * @throws DaoException if failed to delete entity by ID.
      */
-    void delete(T entity) throws DaoException, SQLException;
-
-    /**
-     * Finds all entity in the data source.
-     *
-     * @return the collection of found entities.
-     */
-    Collection<T> findAll() throws SQLException, DaoException;
+    void delete(long id) throws DaoException;
 }
