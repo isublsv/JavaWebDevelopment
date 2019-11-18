f0a33861116103edb31f9c0dc6af8b8f151f09fe
Dmitry G <isublsv@gmail.com>
2019-11-18 15:29:41 +0300

    added javadocs

--- hitcher/src/main/java/by/gartsmanovich/hitcher/service/impl/TripServiceImpl.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/service/impl/TripServiceImpl.java
@@ -2,20 +2,33 @@ package by.gartsmanovich.hitcher.service.impl;
 
 import by.gartsmanovich.hitcher.dao.transaction.Transaction;
 import by.gartsmanovich.hitcher.service.TripService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
+ * The realisation of Trip Service interface is used to describe of application
+ * functionality for the trip entity.
+ *
  * @author Dmitry Gartsmanovich
  */
 public class TripServiceImpl implements TripService {
 
     /**
-     * 
+     * The logger for TripServiceImpl class.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(
+            TripServiceImpl.class);
+
+    /**
+     * Represents the transaction entity.
      */
     private Transaction transaction;
 
     /**
-     * 
-     * @param transactionValue
+     * Constructs the Trip Service realisation instance with provided
+     * transaction value.
+     *
+     * @param transactionValue the provided transaction.
      */
     public TripServiceImpl(final Transaction transactionValue) {
         this.transaction = transactionValue;
