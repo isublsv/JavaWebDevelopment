72782ec9ffab2bf28d1b9fdaf8d50af3378fd676
isublsv <isublsv@gmail.com>
2019-11-21 21:38:13 +0300

    added realisation

--- hitcher/src/main/java/by/gartsmanovich/hitcher/controller/action/impl/LoginActionCommand.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/controller/action/impl/LoginActionCommand.java
@@ -1,12 +1,16 @@
 package by.gartsmanovich.hitcher.controller.action.impl;
 
+import by.gartsmanovich.hitcher.bean.User;
 import by.gartsmanovich.hitcher.controller.action.ActionCommand;
+import by.gartsmanovich.hitcher.service.UserService;
+import by.gartsmanovich.hitcher.service.exception.ServiceException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -37,7 +41,22 @@ public class LoginActionCommand extends ActionCommand {
      *                          could not be handled
      */
     @Override
-    public void execute(final HttpServletRequest request,
-            final HttpServletResponse response) {
+    public void execute(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("pass");
+        if (login != null && pass != null) {
+            UserService userService = getFactory().getUserService();
+            try {
+                User user = userService.findByLoginAndPassword(login, pass);
+                HttpSession session = request.getSession();
+                session.setAttribute("authorizedUser", user);
+            } catch (ServiceException e) {
+                String message = e.getMessage();
+                LOGGER.warn(message);
+                request.setAttribute("errorMessage", message);
+                request.getRequestDispatcher("/WEB-INF/jsp/error/error.jsp")
+                       .forward(request, response);
+            }
+        }
     }
 }
