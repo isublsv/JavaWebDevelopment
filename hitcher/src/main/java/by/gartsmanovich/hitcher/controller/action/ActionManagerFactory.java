b76e59105672af86bed913a81df3d0584239814e
Dmitry G <isublsv@gmail.com>
2019-11-21 14:46:21 +0300

    added javadocs

---
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/controller/action/ActionManagerFactory.java
@@ -0,0 +1,29 @@
+package by.gartsmanovich.hitcher.controller.action;
+
+import by.gartsmanovich.hitcher.service.factory.ServiceFactory;
+
+/**
+ * Class factory used to create Action manager classes using the provided
+ * Service factory..
+ *
+ * @author Dmitry Gartsmanovich
+ */
+public final class ActionManagerFactory {
+
+    /**
+     * Constructs Action Factory instance.
+     */
+    private ActionManagerFactory() {
+    }
+
+    /**
+     * Returns Action Manager instance using the provided Service factory.
+     *
+     * @param factory the provided Service factory.
+     * @return the Action Manager instance.
+     */
+    public static ActionManager getManager(final ServiceFactory factory) {
+        return new ActionManagerImpl(factory);
+    }
+}
+
