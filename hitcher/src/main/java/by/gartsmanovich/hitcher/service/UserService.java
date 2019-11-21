b5b45a8299326e932e22f1f8f8a97455e683f1e4
Dmitry G <isublsv@gmail.com>
2019-11-21 13:31:59 +0300

    Optional was removed

--- hitcher/src/main/java/by/gartsmanovich/hitcher/service/UserService.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/service/UserService.java
@@ -4,7 +4,6 @@ import by.gartsmanovich.hitcher.bean.User;
 import by.gartsmanovich.hitcher.service.exception.ServiceException;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * The interface is used to determine the common structure of User Service
@@ -26,10 +25,10 @@ public interface UserService {
      * Finds user entity by provided ID value.
      *
      * @param id the provided user ID.
-     * @return the user entity if present.
+     * @return the user entity.
      * @throws ServiceException if failed to find user entity.
      */
-    Optional<User> findByID(long id) throws ServiceException;
+    User findByID(long id) throws ServiceException;
 
     /**
      * Deletes user entity by ID from the data source.
@@ -49,14 +48,14 @@ public interface UserService {
     List<User> findAll() throws ServiceException;
 
     /**
-     * Finds user entity by login and password value if present.
+     * Finds user entity by login and password value.
      *
      * @param login    the provided user login.
      * @param password the provided user password.
-     * @return the user entity if present.
+     * @return the user entity.
      * @throws ServiceException if failed to find user entity in the
      *                          data source.
      */
-    Optional<User> findByLoginAndPassword(String login, String password) throws
+    User findByLoginAndPassword(String login, String password) throws
             ServiceException;
 }
