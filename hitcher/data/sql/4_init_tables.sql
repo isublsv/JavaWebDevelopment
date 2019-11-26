50c36ff555a11cc6a47fbf28e8973aec2865c3f1
Dmitry G <isublsv@gmail.com>
2019-11-26 13:55:32 +0300

    pass hash change

--- hitcher/data/sql/4_init_tables.sql
+++ hitcher/data/sql/4_init_tables.sql
@@ -7,6 +7,6 @@ INSERT INTO `users` (`id`,
                      `role`)
 VALUES (1,
         "admin",
-        "HJ0/0LX804NQ3L5YXH6BQRPLPQFWG3JXFSHITCCLWRI=", /* PBKDF2WithHmacSHA1 hash пароля "password" */
-        "QNVSQFN2ZKIHYB9EMN28AGNMHC9KBN", /* salt of "password" */
+        "AZS4ST2SLGI/EGDRGCKQ4ISU3XV5VZ0OSUNMXPRPWKI=", /* PBKDF2WithHmacSHA1 hash пароля "password" */
+        "PEMGI4IBOSMWIO98OIIT36F1IEEE85", /* salt of "password" */
         0);
