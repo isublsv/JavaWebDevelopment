b3d3cc85cc3d4168232baa8d874dc50014836a4e
Dmitry G <isublsv@gmail.com>
2019-11-26 21:07:10 +0300

    added javadocs

--- hitcher/src/main/java/by/gartsmanovich/hitcher/action/impl/LogoutActionCommand.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/action/impl/LogoutActionCommand.java
@@ -1,16 +1,17 @@
 package by.gartsmanovich.hitcher.action.impl;
 
-import by.gartsmanovich.hitcher.action.ActionCommand;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
+ * Class describes logout action command that used to invalidate
+ * {@link javax.servlet.http.HttpSession} and redirect user to the index page.
+ *
  * @author Dmitry Gartsmanovich
  */
-public class LogoutActionCommand extends ActionCommand {
+public class LogoutActionCommand extends AuthorizedActionCommand {
+
     /**
      * Handles the request and response and invoke appropriate method in the
      * Service Layer.
@@ -20,13 +21,10 @@ public class LogoutActionCommand extends ActionCommand {
      * @throws IOException      if an input or output error is
      *                          detected when the servlet handles
      *                          the request
-     * @throws ServletException if the request for the GET or POST
-     *                          could not be handled
      */
     @Override
     public void execute(final HttpServletRequest request,
-            final HttpServletResponse response) throws IOException,
-            ServletException {
+            final HttpServletResponse response) throws IOException {
         request.getSession(false).invalidate();
         response.sendRedirect("/index.jsp");
     }
