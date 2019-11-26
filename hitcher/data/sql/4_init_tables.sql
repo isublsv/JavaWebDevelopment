92713ee22acace3dcf696748491caebe43edf60e
Dmitry G <isublsv@gmail.com>
2019-11-26 18:09:50 +0300

    1. moved email to users table 2. added status column

--- hitcher/data/sql/4_init_tables.sql
+++ hitcher/data/sql/4_init_tables.sql
@@ -1,12 +1,5 @@
 USE `hitcher_db`;
 
-INSERT INTO `users` (`id`,
-                     `login`,
-                     `password`,
-                     `salt`,
-                     `role`)
-VALUES (1,
-        "admin",
-        "fdGKFhv5EiPhcaRCILwuG0wfS6dBVN/PJH0QNqKrnCM=", /* PBKDF2WithHmacSHA1 hash пароля "password" */
-        "z9BryTA8A2JSenyVBXQrduaAFq92m8", /* salt of "password" */
-        0);
+/*hash пароля "password" */
+INSERT INTO `users` (`id`, `login`, `email`, `password`, `salt`, `role`, `status`)
+VALUES (1, 'admin', 'admin@mail.by', 'fdGKFhv5EiPhcaRCILwuG0wfS6dBVN/PJH0QNqKrnCM=', 'z9BryTA8A2JSenyVBXQrduaAFq92m8', 0, 1);
