d409a33f54d72cbe9fdacc47aeb804d982817057
Dmitry G <isublsv@gmail.com>
2019-11-29 17:37:51 +0300

    added realisation

--- hitcher/src/main/java/by/gartsmanovich/hitcher/action/impl/ProfileActionCommand.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/action/impl/ProfileActionCommand.java
@@ -1,19 +1,31 @@
 package by.gartsmanovich.hitcher.action.impl;
 
 import by.gartsmanovich.hitcher.action.manager.ConfigurationManager;
+import by.gartsmanovich.hitcher.bean.User;
+import by.gartsmanovich.hitcher.service.UserService;
+import by.gartsmanovich.hitcher.service.exception.ServiceException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
- * Class describes profile action command that used to display information
+ * Class describes profile action command that used to display full information
  * about user.
  *
  * @author Dmitry Gartsmanovich
  */
 public class ProfileActionCommand extends AuthorizedActionCommand {
+
+    /**
+     * The logger for ProfileActionCommand class.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(
+            ProfileActionCommand.class);
 
     /**
      * Handles the request and response and invoke appropriate method in the
@@ -31,8 +43,24 @@ public class ProfileActionCommand extends AuthorizedActionCommand {
     public void execute(final HttpServletRequest request,
             final HttpServletResponse response) throws IOException,
             ServletException {
-       request.getServletContext().getRequestDispatcher(
-               ConfigurationManager.getProperty("path.page.profile"))
-              .forward(request, response);
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("authorizedUser");
+
+        try {
+            UserService userService = getFactory().getUserService();
+            userService.findByID(user.getId());
+            session.setAttribute("authorizedUser", user);
+            request.getServletContext().getRequestDispatcher(
+                    ConfigurationManager.getProperty("path.page.profile"))
+                   .forward(request, response);
+        } catch (ServiceException e) {
+            String message = e.getMessage();
+            LOGGER.warn(message);
+            request.setAttribute("errorMessage", message);
+            request.getRequestDispatcher(
+                    ConfigurationManager.getProperty("path.page.error"))
+                   .forward(request, response);
+        }
+
     }
 }
