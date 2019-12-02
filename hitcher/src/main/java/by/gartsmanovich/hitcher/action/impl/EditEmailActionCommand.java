73fa674f26c4974ce8e57458720b0b93f9f7fcb3
Dmitry G <isublsv@gmail.com>
2019-12-02 20:58:56 +0300

    added realisation

---
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/action/impl/EditEmailActionCommand.java
@@ -0,0 +1,71 @@
+package by.gartsmanovich.hitcher.action.impl;
+
+import by.gartsmanovich.hitcher.action.manager.ConfigurationManager;
+import by.gartsmanovich.hitcher.bean.User;
+import by.gartsmanovich.hitcher.service.UserService;
+import by.gartsmanovich.hitcher.service.exception.ServiceException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Class describes edit personal data action command that used to edit
+ * a personal user information such as name, surname, patronymic, phone
+ * and address.
+ *
+ * @author Dmitry Gartsmanovich
+ */
+public class EditEmailActionCommand extends AuthorizedActionCommand {
+
+    /**
+     * The logger for EditEmailActionCommand class.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(
+            EditEmailActionCommand.class);
+
+    /**
+     * Handles the request and response and invoke appropriate method in the
+     * Service Layer.
+     *
+     * @param request  the provided request information for HTTP servlets.
+     * @param response the provided response information for HTTP servlets.
+     * @throws IOException      if an input or output error is
+     *                          detected when the servlet handles
+     *                          the request
+     * @throws ServletException if the request for the GET or POST
+     *                          could not be handled
+     */
+    @Override
+    public void execute(final HttpServletRequest request,
+            final HttpServletResponse response) throws IOException,
+            ServletException {
+
+        HttpSession session = request.getSession();
+        String email = request.getParameter("email");
+        User authorizedUser = (User) session.getAttribute("authorizedUser");
+
+        try {
+            UserService userService = getFactory().getUserService();
+            User updatedUser = userService.updateEmail(authorizedUser.getId(),
+                                                       email);
+            session.setAttribute("authorizedUser", updatedUser);
+            LOGGER.debug("Personal user email were updated successfully");
+            request.getServletContext()
+                   .getRequestDispatcher(ConfigurationManager.getProperty(
+                           "path.page.profile"))
+                   .forward(request, response);
+        } catch (ServiceException e) {
+            String message = e.getCode().getMessage();
+            LOGGER.warn(message);
+            request.setAttribute("errorMessage", message);
+            request.getRequestDispatcher(
+                    ConfigurationManager.getProperty("path.page.error"))
+                   .forward(request, response);
+        }
+    }
+}
