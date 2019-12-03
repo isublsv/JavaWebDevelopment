a528b0c533c185dfc2e99cab3a4dc7b0316655ad
Dmitry G <isublsv@gmail.com>
2019-12-04 00:24:23 +0300

    added findAllReviewsByWhoId and findAllReviewsByAboutId methods realisation

--- hitcher/src/main/java/by/gartsmanovich/hitcher/service/ReviewService.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/service/ReviewService.java
@@ -1,9 +1,10 @@
 package by.gartsmanovich.hitcher.service;
 
 import by.gartsmanovich.hitcher.bean.Review;
+import by.gartsmanovich.hitcher.bean.User;
 import by.gartsmanovich.hitcher.service.exception.ServiceException;
 
-import java.util.List;
+import java.util.Map;
 
 /**
  * The interface is used to determine the common structure of Review Service
@@ -14,11 +15,20 @@ import java.util.List;
 public interface ReviewService {
 
     /**
-     * Finds all reviews about user by ID.
+     * Finds all reviews by author ID.
+     *
+     * @param id the provided author ID.
+     * @return the review list.
+     * @throws ServiceException if failed to find reviews by author ID.
+     */
+    Map<Review, User> findReviewsByWhoID(long id) throws ServiceException;
+
+    /**
+     * Finds all reviews about provided user ID.
      *
      * @param id the provided user ID.
      * @return the review list.
      * @throws ServiceException if failed to find reviews by user ID.
      */
-    List<Review> findReviewsByID(long id) throws ServiceException;
+    Map<Review, User> findReviewsByAboutID(long id) throws ServiceException;
 }
--- hitcher/src/main/java/by/gartsmanovich/hitcher/service/impl/ReviewServiceImpl.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/service/impl/ReviewServiceImpl.java
@@ -1,9 +1,20 @@
 package by.gartsmanovich.hitcher.service.impl;
 
+import by.gartsmanovich.hitcher.bean.Review;
+import by.gartsmanovich.hitcher.bean.User;
+import by.gartsmanovich.hitcher.dao.ReviewDao;
+import by.gartsmanovich.hitcher.dao.UserDao;
+import by.gartsmanovich.hitcher.dao.exception.DaoException;
 import by.gartsmanovich.hitcher.dao.transaction.Transaction;
 import by.gartsmanovich.hitcher.service.ReviewService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import by.gartsmanovich.hitcher.service.exception.ServiceException;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static by.gartsmanovich.hitcher.service.exception.ServiceErrorCodes.SQL_ERROR;
 
 /**
  * The realisation of Review Service interface is used to describe of
@@ -12,12 +23,6 @@ import org.apache.logging.log4j.Logger;
  * @author Dmitry Gartsmanovich
  */
 public class ReviewServiceImpl implements ReviewService {
-
-    /**
-     * The logger for ReviewServiceImpl class.
-     */
-    private static final Logger LOGGER = LogManager.getLogger(
-            ReviewServiceImpl.class);
 
     /**
      * Represents the transaction entity.
@@ -32,5 +37,59 @@ public class ReviewServiceImpl implements ReviewService {
      */
     public ReviewServiceImpl(final Transaction transactionValue) {
         this.transaction = transactionValue;
+    }
+
+    /**
+     * Finds all reviews about user by ID.
+     *
+     * @param id the provided user ID.
+     * @return the review list.
+     * @throws ServiceException if failed to find reviews by user ID.
+     */
+    @Override
+    public Map<Review, User> findReviewsByWhoID(final long id) throws
+            ServiceException {
+        ReviewDao reviewDao = transaction.getReviewDao();
+        Map<Review, User> reviewUserMap = new LinkedHashMap<>();
+        try {
+            List<Review> reviewList = reviewDao.findAllReviewsByWhoId(id);
+            UserDao userDao = transaction.getUserDao();
+            for (Review review : reviewList) {
+                Optional<User> user = userDao.findById(review.getAboutId());
+                user.ifPresent(
+                        userValue -> reviewUserMap.put(review, userValue));
+            }
+        } catch (DaoException e) {
+            throw new ServiceException(SQL_ERROR);
+        }
+
+        return reviewUserMap;
+    }
+
+    /**
+     * Finds all reviews about provided user ID.
+     *
+     * @param id the provided user ID.
+     * @return the review list.
+     * @throws ServiceException if failed to find reviews by user ID.
+     */
+    @Override
+    public Map<Review, User> findReviewsByAboutID(final long id) throws
+            ServiceException {
+        ReviewDao reviewDao = transaction.getReviewDao();
+        Map<Review, User> reviewUserMap = new LinkedHashMap<>();
+        try {
+            List<Review> reviewList = reviewDao.findAllReviewsByAboutId(id);
+            UserDao userDao = transaction.getUserDao();
+            for (Review review : reviewList) {
+                Optional<User> user = userDao.findById(review.getWhoId());
+                user.ifPresent(
+                        userValue -> reviewUserMap.put(review, userValue));
+            }
+        } catch (DaoException e) {
+            throw new ServiceException(SQL_ERROR);
+        }
+
+        return reviewUserMap;
     }
 }
