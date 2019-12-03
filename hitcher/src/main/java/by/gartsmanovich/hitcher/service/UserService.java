3d80db8e3d42f268a4606bf41872e826a4284115
Dmitry G <isublsv@gmail.com>
2019-12-03 18:48:25 +0300

    added several methods

--- hitcher/src/main/java/by/gartsmanovich/hitcher/service/UserService.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/service/UserService.java
@@ -20,7 +20,7 @@ public interface UserService {
      * @param login the provided entity login value
      * @param email the provided entity email value
      * @param pass  the provided entity password value
-     * @return the new user value
+     * @return the new user entity.
      * @throws ServiceException if failed to create user entity in the data
      *                          source.
      */
@@ -30,10 +30,62 @@ public interface UserService {
      * Updates personal user information in the data source.
      *
      * @param user the provided user entity with a new personal information.
+     * @return the updated user entity.
      * @throws ServiceException if failed to update user entity in the data
      *                          source.
      */
-    void updatePersonalData(User user) throws ServiceException;
+    User updatePersonalData(User user) throws ServiceException;
+
+    /**
+     * Updates music and communication user preferences in the data source.
+     *
+     * @param id            the provided user ID.
+     * @param music         the provided music preferences.
+     * @param communication the provided communication preferences.
+     * @return the updated user entity.
+     * @throws ServiceException if failed to update user entity in the data
+     *                          source.
+     */
+    User updatePreferences(long id, String music, String communication) throws
+            ServiceException;
+
+    /**
+     * Updates user driver information in the data source.
+     *
+     * @param id       the provided user ID.
+     * @param license  the provided driver license.
+     * @param carModel the provided car model.
+     * @param carColor the provided car color.
+     * @return the updated user entity.
+     * @throws ServiceException if failed to update user entity in the data
+     *                          source.
+     */
+    User updateDriverInfo(long id, String license, String carModel,
+            String carColor) throws ServiceException;
+
+    /**
+     * Updates user email in the data source.
+     *
+     * @param id    the provided user ID.
+     * @param email the provided email value.
+     * @return the updated user entity.
+     * @throws ServiceException if failed to update user entity in the data
+     *                          source.
+     */
+    User updateEmail(long id, String email) throws ServiceException;
+
+    /**
+     * Updates user password in the data source.
+     *
+     * @param id          the provided user ID.
+     * @param currentPass the provided current password value.
+     * @param newPass     the provided new password value.
+     * @return the updated user entity.
+     * @throws ServiceException if failed to update user entity in the data
+     *                          source.
+     */
+    User updatePassword(long id, String currentPass, String newPass) throws
+            ServiceException;
 
     /**
      * Finds user entity by provided ID value.
@@ -73,4 +125,5 @@ public interface UserService {
      */
     User findByLoginAndPassword(String login, String password) throws
             ServiceException;
+
 }
