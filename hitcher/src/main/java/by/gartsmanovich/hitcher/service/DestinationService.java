e168e00b12b1a3e1f3e2a3cc113295fd2e5e092e
Dmitry G <isublsv@gmail.com>
2019-12-04 23:47:36 +0300

    added realisation

--- hitcher/src/main/java/by/gartsmanovich/hitcher/service/DestinationService.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/service/DestinationService.java
@@ -1,6 +1,7 @@
 package by.gartsmanovich.hitcher.service;
 
 import by.gartsmanovich.hitcher.bean.Destination;
+import by.gartsmanovich.hitcher.service.exception.ServiceException;
 
 import java.util.List;
 
@@ -16,6 +17,8 @@ public interface DestinationService {
      * Finds all possible destination in the data source.
      *
      * @return the list of destinations.
+     * @throws ServiceException if failed to find all destination entities in
+     *                          the data source.
      */
-    List<Destination> findAll();
+    List<Destination> findAll() throws ServiceException;
 }
---
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/service/impl/DestinationServiceImpl.java
@@ -0,0 +1,52 @@
+package by.gartsmanovich.hitcher.service.impl;
+
+import by.gartsmanovich.hitcher.bean.Destination;
+import by.gartsmanovich.hitcher.dao.DestinationDao;
+import by.gartsmanovich.hitcher.dao.exception.DaoException;
+import by.gartsmanovich.hitcher.dao.transaction.Transaction;
+import by.gartsmanovich.hitcher.service.DestinationService;
+import by.gartsmanovich.hitcher.service.exception.ServiceErrorCodes;
+import by.gartsmanovich.hitcher.service.exception.ServiceException;
+
+import java.util.List;
+
+/**
+ * The realisation of Destination Service interface is used to describe of
+ * application functionality for the destination entity.
+ *
+ * @author Dmitry Gartsmanovich
+ */
+public class DestinationServiceImpl implements DestinationService {
+
+    /**
+     * Represents the transaction entity.
+     */
+    private Transaction transaction;
+
+    /**
+     * Constructs the Destination Service realisation instance with provided
+     * transaction value.
+     *
+     * @param transactionValue the provided transaction.
+     */
+    public DestinationServiceImpl(final Transaction transactionValue) {
+        this.transaction = transactionValue;
+    }
+
+    /**
+     * Finds all possible destination in the data source.
+     *
+     * @return the list of destinations.
+     * @throws ServiceException if failed to find all destination entities in
+     *                          the data source.
+     */
+    @Override
+    public List<Destination> findAll() throws ServiceException {
+        DestinationDao dao = transaction.getDestinationDao();
+        try {
+            return dao.findAll();
+        } catch (DaoException e) {
+            throw new ServiceException(e, ServiceErrorCodes.SQL_ERROR);
+        }
+    }
+}
