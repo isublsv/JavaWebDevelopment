022a4464bf4d0107c7cd660fe6974aa5e0077792
Dmitry G <isublsv@gmail.com>
2019-12-07 01:59:23 +0300

    added realisation

--- hitcher/src/main/java/by/gartsmanovich/hitcher/action/impl/OfferTripActionCommand.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/action/impl/OfferTripActionCommand.java
@@ -1,9 +1,18 @@
 package by.gartsmanovich.hitcher.action.impl;
 
+import by.gartsmanovich.hitcher.action.manager.ConfigurationManager;
+import by.gartsmanovich.hitcher.bean.User;
+import by.gartsmanovich.hitcher.service.TripService;
+import by.gartsmanovich.hitcher.service.exception.ServiceException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Class describes offer trip action command that used to create a new trip
@@ -12,6 +21,13 @@ import java.io.IOException;
  * @author Dmitry Gartsmanovich
  */
 public class OfferTripActionCommand extends AuthorizedActionCommand {
+
+    /**
+     * The logger for OfferTripActionCommand class.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(
+            OfferTripActionCommand.class);
+
     /**
      * Handles the request and response and invoke appropriate method in the
      * Service Layer.
@@ -28,5 +44,28 @@ public class OfferTripActionCommand extends AuthorizedActionCommand {
     public void execute(final HttpServletRequest request,
             final HttpServletResponse response) throws IOException,
             ServletException {
+
+        Map<String, String[]> parameterMap = request.getParameterMap();
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("authorizedUser");
+
+        try {
+            TripService tripService = getFactory().getTripService();
+            tripService.save(user.getId(), parameterMap);
+            LOGGER.debug("Trip was created successfully");
+
+            request.getServletContext()
+                   .getRequestDispatcher(ConfigurationManager.getProperty(
+                           "path.page.my.trips"))
+                   .forward(request, response);
+        } catch (ServiceException e) {
+            String message = e.getCode().getMessage();
+            LOGGER.warn(message);
+            request.setAttribute("errorMessage", message);
+            request.getRequestDispatcher(
+                    ConfigurationManager.getProperty("path.page.error"))
+                   .forward(request, response);
+        }
     }
 }
