5803ce120d7984a30d7dd4a01119c49251a2f7dc
Dmitry G <isublsv@gmail.com>
2019-12-06 16:48:22 +0300

    added a stub

---
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/action/impl/OfferTripActionCommand.java
@@ -0,0 +1,32 @@
+package by.gartsmanovich.hitcher.action.impl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Class describes offer trip action command that used to create a new trip
+ * with additional parameters.
+ *
+ * @author Dmitry Gartsmanovich
+ */
+public class OfferTripActionCommand extends AuthorizedActionCommand {
+    /**
+     * Handles the request and response and invoke appropriate method in the
+     * Service Layer.
+     *
+     * @param request  the provided request information for HTTP servlets.
+     * @param response the provided response information for HTTP servlets.
+     * @throws IOException      if an input or output error is
+     *                          detected when the servlet handles
+     *                          the request
+     * @throws ServletException if the request for the GET or POST
+     *                          could not be handled
+     */
+    @Override
+    public void execute(final HttpServletRequest request,
+            final HttpServletResponse response) throws IOException,
+            ServletException {
+    }
+}
