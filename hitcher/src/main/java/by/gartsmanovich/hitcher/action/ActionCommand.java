a7512b4baee542548714b9de7061157725d38b4f
Dmitry G <isublsv@gmail.com>
2019-12-15 17:26:59 +0300

    refactoring

--- hitcher/src/main/java/by/gartsmanovich/hitcher/action/ActionCommand.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/action/ActionCommand.java
@@ -1,7 +1,11 @@
 package by.gartsmanovich.hitcher.action;
 
+import by.gartsmanovich.hitcher.action.manager.ConfigurationManager;
 import by.gartsmanovich.hitcher.bean.Role;
+import by.gartsmanovich.hitcher.service.exception.ServiceException;
 import by.gartsmanovich.hitcher.service.factory.ServiceFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +20,12 @@ import java.util.Set;
  * @author Dmitry Gartsmanovich
  */
 public abstract class ActionCommand {
+
+    /**
+     * The logger for ActionCommand class.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(
+            ActionCommand.class);
 
     /**
      * Represents Service factory.
@@ -67,5 +77,29 @@ public abstract class ActionCommand {
      */
     public Set<Role> getAllowRoles() {
         return allowRoles;
+    }
+
+    /**
+     * Logs the exception message and forwards to the error page.
+     *
+     * @param request  the provided request information for HTTP servlets.
+     * @param response the provided response information for HTTP servlets.
+     * @param e        the provided exception value that appears during command
+     *                 execution.
+     * @throws IOException      if an input or output error is
+     *                          detected when the servlet handles
+     *                          the request
+     * @throws ServletException if the request for the GET or POST
+     *                          could not be handled
+     */
+    public void processError(final HttpServletRequest request,
+            final HttpServletResponse response,
+            final ServiceException e) throws ServletException, IOException {
+        String message = e.getErrorCode().getMessage();
+        LOGGER.warn(message);
+        request.setAttribute("errorMessage", message);
+        request.getRequestDispatcher(
+                ConfigurationManager.getProperty("path.page.error")).forward(
+                request, response);
     }
 }
