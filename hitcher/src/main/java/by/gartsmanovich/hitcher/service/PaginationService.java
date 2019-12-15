5eb8b3b6a0fb25b48f9594bdb9c9ebd192ffcaab
Dmitry G <isublsv@gmail.com>
2019-12-15 17:06:24 +0300

    fixed javadoc

--- hitcher/src/main/java/by/gartsmanovich/hitcher/service/PaginationService.java
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/service/PaginationService.java
@@ -3,6 +3,10 @@ package by.gartsmanovich.hitcher.service;
 import by.gartsmanovich.hitcher.bean.Entity;
 
 /**
+ * Interface used to describe the common structure of services that support
+ * pagination.
+ *
+ * @param <T> the type of elements which the service can support.
  * @author Dmitry Gartsmanovich
  */
 public interface PaginationService<T extends Entity> {
