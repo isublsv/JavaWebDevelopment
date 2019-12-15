5758a9f23b0a5c662ce4443ae0e92e86b061815e
Dmitry G <isublsv@gmail.com>
2019-12-15 10:34:53 +0300

    added  realisation

---
+++ hitcher/src/main/java/by/gartsmanovich/hitcher/action/impl/TripUnregisteringActionCommand.java
@@ -0,0 +1,74 @@
+package by.gartsmanovich.hitcher.action.impl;
+
+import by.gartsmanovich.hitcher.action.manager.ConfigurationManager;
+import by.gartsmanovich.hitcher.bean.User;
+import by.gartsmanovich.hitcher.service.TripService;
+import by.gartsmanovich.hitcher.service.exception.ServiceException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+import static by.gartsmanovich.hitcher.service.exception.ServiceErrorCodes
+        .INVALID_PARAMETER_VALUE;
+
+/**
+ * Class describes unregister from a trip action command.
+ *
+ * @author Dmitry Gartsmanovich
+ */
+public class TripUnregisteringActionCommand extends AuthorizedActionCommand {
+
+    /**
+     * The logger for TripUnregisteringActionCommand class.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(
+            TripUnregisteringActionCommand.class);
+
+    /**
+     * Handles the request and response and invoke appropriate method in the
+     * Service Layer.
+     *
+     * @param request  the provided request information for HTTP servlets.
+     * @param response the provided response information for HTTP servlets.
+     * @throws IOException      if an input or output error is
+     *                          detected when the servlet handles
+     *                          the request
+     * @throws ServletException if the request for the GET or POST
+     *                          could not be handled
+     */
+    @Override
+    public void execute(final HttpServletRequest request,
+            final HttpServletResponse response) throws IOException,
+            ServletException {
+
+        String tripId = request.getParameter("id");
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("authorizedUser");
+
+        try {
+            if (tripId != null) {
+                TripService tripService = getFactory().getTripService();
+                tripService.deletePassenger(user.getId(), tripId);
+                LOGGER.debug("Unregister from a trip was completed"
+                             + " successfully");
+                String redirect = request.getHeader("referer");
+                response.sendRedirect(redirect);
+            } else {
+                throw new ServiceException(INVALID_PARAMETER_VALUE);
+            }
+        } catch (ServiceException e) {
+            String message = e.getErrorCode().getMessage();
+            LOGGER.warn(message);
+            request.setAttribute("errorMessage", message);
+            request.getRequestDispatcher(
+                    ConfigurationManager.getProperty("path.page.error"))
+                   .forward(request, response);
+        }
+    }
+}
